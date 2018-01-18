aefcb5f07c9f3d10521754d2f1d001fefd9593c5
huskyhoochu <dfg1499@gmail.com>
2018-01-18 20:39:44 +0900

    practice update

--- Comments, Print Statements and Debug Console.playground/Contents.swift
+++ Comments, Print Statements and Debug Console.playground/Contents.swift
@@ -1,5 +1,22 @@
 //: Playground - noun: a place where people can play
 
+// 짧은 주석
+
+/*
+ 긴 글 주석
+ */
+
 import UIKit
 
 var str = "Hello, playground"
+
+print(str)
+
+var monsterHealth = 19
+
+monsterHealth = monsterHealth + 22
+
+monsterHealth = monsterHealth / 2
+
+print(monsterHealth)
+
